f866fec9e553f825b2f0c4e3b4eea1789c24f0f7
pharmav <pharmavcom@gmail.com>
2022-11-09 17:22:57 +0000

    read / write / subscription watch

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,41 +1,37 @@
 import { writable, get } from 'svelte/store';
-import type { Writable, Subscriber, Unsubscriber } from 'svelte/store';
+import type { Writable, Readable, Subscriber, Unsubscriber } from 'svelte/store';
 
-export type subscriptionWatcher = (hasSubscriber?: boolean) => any;
-
-export default abstract class CustomStore<T> {
-	protected _store: Writable<T>;
+export class CustomStore<T> {
+	$hasSubscriber: SubscriberStore | false = false;
+	$store: Writable<T> | Readable<T>;
 	protected _unsubscribes: (Unsubscriber | null)[] = [];
-	protected _subscriptionWatches: subscriptionWatcher[] = [];
 
-	constructor(value: T) {
+	constructor(value: T, isWritable: boolean = true) {
 		let _this = this;
-		this._store = writable<T>(value, function start() {
-			_this._subscriptionChanged(true);
+		let $store = writable<T>(value, function start() {
+			if (_this.$hasSubscriber) {
+				_this.$hasSubscriber.value = true;
+			}
 			return function stop() {
 				_this._unsubscribes = [];
-				_this._subscriptionChanged(false);
+				if (_this.$hasSubscriber) {
+					_this.$hasSubscriber.value = false;
+				}
 			};
 		});
+
+		if (isWritable) {
+			this.$store = $store;
+		} //
+		else {
+			this.$store = { subscribe: $store.subscribe };
+		}
+
 		return this;
 	}
 
-	get store(): Writable<T> {
-		return this._store;
-	}
-
 	get value(): T {
-		return get(this._store);
-	}
-
-	set value(value: T) {
-		this._store.set(value);
-	}
-
-	protected _subscriptionChanged(hasSubscriber: boolean): void {
-		for (let i = 0, iLen = this._subscriptionWatches.length; i < iLen; i++) {
-			this._subscriptionWatches[i](hasSubscriber);
-		}
+		return get(this.$store);
 	}
 
 	protected _unsubscribe(index: number): void {
@@ -46,18 +42,21 @@ export default abstract class CustomStore<T> {
 	}
 
 	subscribe(callback: Subscriber<T>): Unsubscriber {
-		let _this = this;
-		let unsubscribe: Unsubscriber = this._store.subscribe(callback);
+		let unsubscribe: Unsubscriber = this.$store.subscribe(callback);
 		let unsubscribeIndex = this._unsubscribes.length;
 		this._unsubscribes.push(unsubscribe);
+		let _this = this;
 		return () => {
 			_this._unsubscribe(unsubscribeIndex);
 		};
 	}
 
-	destroy(): void {
+	purge(): void {
 		for (let i = 0, iLen = this._unsubscribes.length; i < iLen; i++) {
 			this._unsubscribe(i);
+		}
+		if (this.$hasSubscriber) {
+			this.$hasSubscriber.purge();
 		}
 		let properties = Object.getOwnPropertyNames(this);
 
@@ -65,8 +64,35 @@ export default abstract class CustomStore<T> {
 			delete this[properties[i] as keyof this];
 		}
 	}
-	subscriptionChange(callback: subscriptionWatcher): this {
-		this._subscriptionWatches.push(callback);
+}
+
+class SubscriberStore extends CustomStore<boolean> {
+	declare $store: Writable<boolean>;
+	constructor(value: boolean) {
+		super(value);
+		return this;
+	}
+	set value(value: boolean) {
+		this.$store.set(value);
+	}
+}
+
+export class WritableStore<T> extends CustomStore<T> {
+	declare $store: Writable<T>;
+	$hasSubscriber: SubscriberStore = new SubscriberStore(false);
+	constructor(value: T) {
+		super(value);
+		return this;
+	}
+	set value(value: T) {
+		this.$store.set(value);
+	}
+}
+export class ReadableStore<T> extends CustomStore<T> {
+	declare $store: Readable<T>;
+	$hasSubscriber: SubscriberStore = new SubscriberStore(false);
+	constructor(value: T) {
+		super(value);
 		return this;
 	}
 }
